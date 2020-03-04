ab17d974fcb12f5d77869d7ca7478ff74f5f0869
TristanRollond <36910812+TristanRollond@users.noreply.github.com>
2020-03-05 08:52:28 +1030

    Added PlayerController Script
    This is a script for basic wasd movement in 3d place

---
+++ PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int walkSpeed;
+    public int runSpeed;
+    int speed;
+    public Rigidbody rb;
+    Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        speed = walkSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Run"))
+        {
+            speed = runSpeed;
+        } else
+        {
+            speed = walkSpeed;
+        }
+        Move();
+	}
+
+    void Move()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+    }
+}
