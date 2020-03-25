9cb177c51eba9092e87fb31788903bf9a8ed8d37
TristanRollond <36910812+TristanRollond@users.noreply.github.com>
2020-03-25 16:21:40 +1030

    Raycasting setup

---
+++ RaycastGun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastGun : MonoBehaviour
+{
+    public float range;
+    public int damage;
+
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
